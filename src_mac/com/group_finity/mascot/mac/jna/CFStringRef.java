c508f77bb0e5a71a9df6ab6e356273e15172e94e
AlanJager <873863981@qq.com>
2016-12-05 21:55:59 +0800

    add first release

--- src_mac/com/group_finity/mascot/mac/jna/CFStringRef.java
+++
@@ -1,6 +0,0 @@
-package com.group_finity.mascot.mac.jna;
-
-import com.group_finity.mascot.mac.jna.CFTypeRef;
-
-public class CFStringRef extends CFTypeRef {
-}
--- src_mac/com/group_finity/mascot/mac/jna/ProcessSerialNumber.java
+++
@@ -1,7 +0,0 @@
-package com.group_finity.mascot.mac.jna;
-
-import com.sun.jna.Structure;
-
-public class ProcessSerialNumber extends Structure {
-	public long highLongOfPSN, lowLongOfPSN;
-}
